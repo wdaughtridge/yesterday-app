f144a9512b7fc39b4a7901bac5caece1f82d71b8
Will Daughtridge <wdaughtridge@gmail.com>
2023-04-16 02:37:19 -0400

    Versioning untracked files

--- client/app/page.js
+++ client/app/page.js
@@ -6,6 +6,10 @@ import { Canvas } from './Canvas'
 import { AddSource } from './AddSource'
 import { useState } from 'react'
 import { CircularProgress } from '@mui/material';
+import { highlight, languages } from 'prismjs/components/prism-core';
+import 'prismjs/components/prism-clike';
+import 'prismjs/components/prism-javascript';
+import 'prismjs/themes/prism.css';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,6 +21,7 @@ export default function Home() {
   const [jobID, setJobID] = useState(-1);
   const [jobRes, setJobRes] = useState('');
   const [isLoading, setIsLoading] = useState(false);
+  const [code, setCode] = React.useState('# Type your Spark jobs here.');
   async function sendRequest() {
     setIsLoading(true);
     const requestOptions = {
@@ -31,12 +36,12 @@ export default function Home() {
     setIsLoading(false);
   }
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className='py-5'>
+    <main className="flex min-h-screen flex-col items-center justify-between p-16">
+      <div className='py-4'>
         <AddSource className='my-2' boxes={boxes} setBoxes={setBoxes} />
-        <button className="group my-2 rounded-lg border border-neutral-700 bg-neutral-800/30 px-5 py-4 transition-colors hover:border-gray-600 hover:bg-gray-700" onClick={sendRequest}>Test Request</button>
+        <button className="group my-2 rounded-lg border border-neutral-700 bg-neutral-800/30 px-4 py-4 transition-colors hover:border-gray-600 hover:bg-gray-700" onClick={sendRequest}>Test Request</button>
       </div>
-      <div className='py-5'>
+      <div className='py-4'>
         {isLoading ? 
           <CircularProgress /> 
           : (jobID != -1) ? <div>
@@ -46,11 +51,23 @@ export default function Home() {
             </div> : <div />
         }
       </div>
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
+      <div>
+        <Editor
+          value={code}
+          onValueChange={code => setCode(code)}
+          highlight={code => highlight(code, languages.python)}
+          padding={10}
+          style={{
+            fontFamily: '"Fira code", "Fira Mono", monospace',
+            fontSize: 12,
+          }}
+        />
+      </div>
+      {/* <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
         <DndProvider backend={HTML5Backend}>
           <Canvas boxes={boxes} setBoxes={setBoxes} />
         </DndProvider>
-      </div>
+      </div> */}
     </main>
   )
 }
